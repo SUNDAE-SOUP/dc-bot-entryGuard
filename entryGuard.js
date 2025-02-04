915d9339e26bb528245a6e191d778e4ca36d5eee
Rhejoel Joson <rhejoelstrong@gmail.com>
2025-02-05 03:49:25 +0800

    Add files via upload

---
+++ entryGuard.js
@@ -0,0 +1,27 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers
+    ]
+});
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('guildMemberAdd', async (member) => {
+    const roleName = "Mamamayan"; //Change this to the role name you want to assign
+    const role = member.guild.roles.cache.find(r => r.name === roleName);
+
+    if (role) {
+        await member.roles.add(role);
+        console.log(`Assigned ${roleName} role to ${member.user.tag}`);
+    } else {
+        console.log(`Role ${roleName} not found!`);
+    }
+});
+
+client.login(process.env.DISCORD_TOKEN);
